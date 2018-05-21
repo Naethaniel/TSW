49d0b4e9bd9c9cc15d75cb2181a2ad0eb62ce3cc
Naethaniel <bljuuzeff@gmail.com>
2018-05-21 02:01:52 +0200

    Removed bootstrap from project, loading it inside index.html because webpack wont work with me

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   context: path.join(__dirname, 'src'),
@@ -19,34 +20,6 @@ module.exports = {
           'babel-loader',
         ],
       },
-      {
-        test: /\.(scss)$/,
-        use: [
-          {
-            // Adds CSS to the DOM by injecting a `<style>` tag
-            loader: 'style-loader'
-          },
-          {
-            // Interprets `@import` and `url()` like `import/require()` and will resolve them
-            loader: 'css-loader'
-          },
-          {
-            // Loader for webpack to process CSS with PostCSS
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('autoprefixer')
-                ];
-              }
-            }
-          },
-          {
-            // Loads a SASS/SCSS file and compiles it to CSS
-            loader: 'sass-loader'
-          }
-        ]
-      }
     ],
   },
 };
