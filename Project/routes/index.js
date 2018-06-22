220cbf4d832dfedd3eec4fd2d74d8efa7a546063
Naethaniel <bljuuzeff@gmail.com>
2018-06-22 13:52:58 +0200

    Project: -login & register errors are showing on the pages -res.locals.user missing?

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -35,75 +35,74 @@ router.post('/register', (req, res) => {
             });
         }
         else {
-            //checking for username if its already taken
-            User.findOne({
-                username: {
-                    "$regex": "^" + username + "\\b", "$options": "i"
-                }
-            }, function (err, user) {
-                if (user) {
-                    res.render('register', {
-                        user: user
-                    });
-                }
-                else {
-                    var newUser = new User({
-                        username: username,
-                        password: password
-                    });
-                    User.createUser(newUser, function (err, user) {
-                        if (err) throw err;
-                        console.log(user);
-                    });
-                    req.flash('success_msg', 'You are registered and can now login');
-                    res.redirect('login');
-                }
-            });
+          //checking for email and username are already taken
+          User.findOne({
+            username: {
+              "$regex": "^" + username + "\\b", "$options": "i"
+            }
+          }, function (err, user) {
+            if (user) {
+              res.render('register', {
+                user: user,
+              });
+            }
+            else {
+              var newUser = new User({
+                username: username,
+                password: password
+              });
+              User.createUser(newUser, function (err, user) {
+                if (err) throw err;
+                console.log(user);
+              });
+              req.flash('success_msg', 'You are registered and can now login');
+              res.redirect('/login');
+            }
+          });
         }
-    }
-);
+});
 
 passport.use(new LocalStrategy(
-    function (username, password, done) {
-        User.getUserByUsername(username, function (err, user) {
-            if (err) throw err;
-            if (!user) {
-                return done(null, false, {message: 'Unknown User'});
-            }
+  function (username, password, done) {
+    User.getUserByUsername(username, function (err, user) {
+      if (err) throw err;
+      if (!user) {
+        return done(null, false, {message: 'Unknown User'});
+      }
 
-            User.comparePassword(password, user.password, function (err, isMatch) {
-                if (err) throw err;
-                if (isMatch) {
-                    return done(null, user);
-                } else {
-                    return done(null, false, {message: 'Invalid password'});
-                }
-            });
-        });
-    }));
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) throw err;
+        if (isMatch) {
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Invalid password'});
+        }
+      });
+    });
+  }));
 
 passport.serializeUser(function (user, done) {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-    User.getUserById(id, function (err, user) {
-        done(err, user);
-    });
+  User.getUserById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
 router.post('/login',
-    passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true}),
-    function (req, res) {
-        res.redirect('/');
-    });
+  passport.authenticate('local', {successRedirect: '/', failureRedirect: '/login', failureFlash: true}),
+  function (req, res) {
+    res.redirect('/');
+  });
 
 router.get('/logout', function (req, res) {
-    req.logout();
+  req.logout();
 
-    req.flash('success_msg', 'You are logged out');
+  req.flash('success_msg', 'You are logged out');
 
-    res.redirect('/');
+  res.redirect('/users/login');
 });
 
 module.exports = router;
